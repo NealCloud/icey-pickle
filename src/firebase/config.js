e7611c0c344d361c0a2f77b29f350b3159f9c309
NealCloud <Neal.Cloud@gmail.com>
2023-01-09 15:32:09 -0800

    firebase adjust

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import {intializeApp} from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDtSWrMm_e_mw8acGYePpmqmoy4mVkzWnU",
+  
+    authDomain: "pickleballertest.firebaseapp.com",
+  
+    projectId: "pickleballertest",
+  
+    storageBucket: "pickleballertest.appspot.com",
+  
+    messagingSenderId: "924475184929",
+  
+    appId: "1:924475184929:web:c7b54243a7f21fbc3d9060"
+  
+  };
+
+  initializeApp(firebaeConfig)
